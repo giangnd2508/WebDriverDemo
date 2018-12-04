9090741d35875b82804e90a5b04972a379030451
giangnd2508 <giang.nd2508@gmail.com>
2018-12-04 08:06:22 +0700

    read file

---
+++ src/utilities/UploadFile.java
@@ -0,0 +1,13 @@
+package utilities;
+
+import org.openqa.selenium.WebElement;
+
+public class UploadFile {
+	
+	private String path = "";
+	
+	public void uploadFile(WebElement ele, String path) {
+		
+	}
+
+}
