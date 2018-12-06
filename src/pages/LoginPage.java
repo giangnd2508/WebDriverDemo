1f03ce90189d434cf6c3ca0bc91c6626db772950
giangnd2508 <giang.nd2508@gmail.com>
2018-12-06 22:45:40 +0700

    add logging

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,6 +10,8 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.Links;
 
 public class LoginPage {
+	
+	private static final Logger log = LogManager.getLogger(LoginPage.class.getName());
 
 	@FindBy(id = "user_login")
 	WebElement user_login;
@@ -29,12 +33,15 @@ public class LoginPage {
 		user_login.sendKeys(userName);
 		user_pass.sendKeys(password);
 		submitBtn.click();
+		log.info("Login with username: '"+ userName + "' and password: '" + password + "'");
+		
 	}
 	
 	public DashboardPage loginSuccess() {
 		user_login.sendKeys("giang");
 		user_pass.sendKeys("123456789");
 		submitBtn.click();
+		log.info("Get into the dashboard page");
 		return new DashboardPage(driver);
 	}
 
